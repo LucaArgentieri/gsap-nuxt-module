cffcd22e5741aa0c3ca587899e49171f8e898785
LucaArgentieri <lucaargentieri-98@hotmail.it>
2025-07-03 20:53:00 +0200

    Created DrawSvg Demo

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,11 +1,8 @@
 export default defineNuxtConfig({
   modules: ['../src/module'],
-  devtools: { enabled: true },
-  runtimeConfig: {
-    gsap: process.env.NUXT_GSAP_TOKEN,
-  },
+  devtools: { enabled: false },
   compatibilityDate: '2025-02-26',
   gsap: {
-    plugins: ['Draggable', 'EaselPlugin', 'Flip', 'MotionPathPlugin', 'Observer', 'PixiPlugin', 'ScrollToPlugin', 'ScrollTrigger', 'TextPlugin', 'RoughEase', 'ExpoScaleEase', 'SlowMo', 'CustomEase', 'InertiaPlugin', 'ScrollSmoother', 'GSDevTools', 'MotionPathHelper', 'SplitText', 'ScrambleTextPlugin', 'Physics2DPlugin', 'PhysicsPropsPlugin', 'CustomWiggle', 'CustomBounce'],
+    plugins: ['Draggable', 'EaselPlugin', 'Flip', 'MotionPathPlugin', 'Observer', 'PixiPlugin', 'ScrollToPlugin', 'ScrollTrigger', 'TextPlugin', 'RoughEase', 'ExpoScaleEase', 'SlowMo', 'CustomEase', 'InertiaPlugin', 'ScrollSmoother', 'GSDevTools', 'MotionPathHelper', 'SplitText', 'ScrambleTextPlugin', 'Physics2DPlugin', 'PhysicsPropsPlugin', 'CustomWiggle', 'CustomBounce', 'DrawSVGPlugin'],
   },
 })
---
+++ playground/pages/draw-svg.vue
@@ -0,0 +1,61 @@
+<script setup>
+onMounted(() => {
+  gsap.to('#lineSvg', {
+    drawSVG: '0% 50%',
+    duration: 2,
+    repeat: -1,
+    yoyo: true,
+  })
+
+  gsap.from('#circleSvg circle', {
+    drawSVG: '50% 50%',
+    duration: 2,
+    ease: 'power1.inOut',
+    repeat: -1,
+    yoyo: true,
+  })
+})
+</script>
+
+<template>
+  <main>
+    <svg
+      width="200"
+      height="200"
+    >
+      <path
+        id="lineSvg"
+        d="M10 10 L 190 190"
+        stroke="black"
+        stroke-width="2"
+        fill="none"
+      />
+    </svg>
+
+    <svg
+      id="circleSvg"
+      width="200"
+      height="200"
+    >
+      <circle
+        cx="100"
+        cy="100"
+        r="50"
+        stroke="black"
+        stroke-width="4"
+        fill="none"
+      />
+    </svg>
+  </main>
+</template>
+
+<style scoped>
+main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    height: 100svh;
+    overflow: hidden;
+}
+</style>
