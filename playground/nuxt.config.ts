b1e7b7701cae4085ddee2936581314a0574f56f3
LucaArgentieri <lucaargentieri-98@hotmail.it>
2025-07-03 20:57:46 +0200

    created MorphSvg Demo

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -3,6 +3,6 @@ export default defineNuxtConfig({
   devtools: { enabled: false },
   compatibilityDate: '2025-02-26',
   gsap: {
-    plugins: ['Draggable', 'EaselPlugin', 'Flip', 'MotionPathPlugin', 'Observer', 'PixiPlugin', 'ScrollToPlugin', 'ScrollTrigger', 'TextPlugin', 'RoughEase', 'ExpoScaleEase', 'SlowMo', 'CustomEase', 'InertiaPlugin', 'ScrollSmoother', 'GSDevTools', 'MotionPathHelper', 'SplitText', 'ScrambleTextPlugin', 'Physics2DPlugin', 'PhysicsPropsPlugin', 'CustomWiggle', 'CustomBounce', 'DrawSVGPlugin'],
+    plugins: ['Draggable', 'EaselPlugin', 'Flip', 'MotionPathPlugin', 'Observer', 'PixiPlugin', 'ScrollToPlugin', 'ScrollTrigger', 'TextPlugin', 'RoughEase', 'ExpoScaleEase', 'SlowMo', 'CustomEase', 'InertiaPlugin', 'ScrollSmoother', 'GSDevTools', 'MotionPathHelper', 'SplitText', 'ScrambleTextPlugin', 'Physics2DPlugin', 'PhysicsPropsPlugin', 'CustomWiggle', 'CustomBounce', 'DrawSVGPlugin', 'MorphSVGPlugin'],
   },
 })
