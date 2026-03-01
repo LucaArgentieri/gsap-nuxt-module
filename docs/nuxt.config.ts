5813ea9effafe118e84b51e9c22c9865bf2144d6
Luca Argentieri <Lucaargentieri-98@hotmail.it>
2026-03-01 11:12:12 +0100

    added llm config for docus

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,7 +1,11 @@
 export default defineNuxtConfig({
-  extends: ['docus'],
-  css: ['~/assets/css/main.css'],
+  extends: ["docus"],
+  css: ["~/assets/css/main.css"],
   robots: {
     robotsTxt: false,
   },
-})
+  // https://docus.dev/en/ai/llms
+  llms: {
+    domain: "https://lucaargentieri.github.io/gsap-nuxt-module/",
+  },
+});
